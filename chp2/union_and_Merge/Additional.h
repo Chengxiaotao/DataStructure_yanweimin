208251f3d0d93399f4c773ec9cce81b5abade531
Chengxiaotao <37770625+Chengxiaotao@users.noreply.github.com>
2018-12-22 11:09:13 +0800

    Create Additional.h

---
+++ chp2/union_and_Merge/Additional.h
@@ -0,0 +1,11 @@
+#pragma   once
+#ifndef _ADDITIONAL_H_
+#define _ADDITIONAL_H_
+
+#include "E:\c_pjs\L_L\L_L\L_list.h"
+
+void unionL(SqList *La, SqList *Lb);//A∪B
+
+void MergeList(SqList La, SqList Lb, SqList *Lc);//合并a、b并按非减有序排列
+
+#endif
